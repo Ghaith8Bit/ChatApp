0ff12489aff3a8fa382a3729b7fd326af40c28ca
Ghaith8Bit <ghaithhamwi123@outlook.com>
2022-09-03 17:23:47 +0300

    setup layout to main livewire component

--- resources/views/livewire/chat/main.blade.php
+++ resources/views/livewire/chat/main.blade.php
@@ -1,3 +1,17 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Main') }}
+        </h2>
+    </x-slot>
+
+    <div class="chat_container">
+        <div class="chat_list_container">
+            @livewire('chat.chat-list')
+        </div>
+        <div class="chat_box_container">
+            @livewire('chat.chat-box')
+            @livewire('chat.send-message')
+        </div>
+    </div>
 </div>
